86c3c13d28f27bc70a11882c08ff84219bc9f7a4
Jeff Listman <jeffreylistman@gmail.com>
2024-01-01 09:51:48 -0600

    more whitespace cleanup

--- sim/shaman/bloodlust.go
+++ sim/shaman/bloodlust.go
@@ -12,73 +12,72 @@ func (shaman *Shaman) BloodlustActionID() core.ActionID {
 }
 
 func (shaman *Shaman) registerBloodlustCD() {
-	return
-	// if !shaman.SelfBuffs.Bloodlust && !shaman.IsUsingAPL {
-	// 	return
-	// }
-	// actionID := shaman.BloodlustActionID()
+	/* if !shaman.SelfBuffs.Bloodlust && !shaman.IsUsingAPL {
+		return
+	}
+	actionID := shaman.BloodlustActionID()
 
-	// blAuras := []*core.Aura{}
-	// for _, party := range shaman.Env.Raid.Parties {
-	// 	for _, partyMember := range party.Players {
-	// 		blAuras = append(blAuras, core.BloodlustAura(partyMember.GetCharacter(), actionID.Tag))
-	// 	}
-	// }
+	blAuras := []*core.Aura{}
+	for _, party := range shaman.Env.Raid.Parties {
+		for _, partyMember := range party.Players {
+			blAuras = append(blAuras, core.BloodlustAura(partyMember.GetCharacter(), actionID.Tag))
+		}
+	}
 
-	// bloodlustSpell := shaman.RegisterSpell(core.SpellConfig{
-	// 	ActionID: actionID,
-	// 	Flags:    core.SpellFlagAPL,
+	bloodlustSpell := shaman.RegisterSpell(core.SpellConfig{
+		ActionID: actionID,
+		Flags:    core.SpellFlagAPL,
 
-	// 	ManaCost: core.ManaCostOptions{
-	// 		BaseCost:   0.26,
-	// 		Multiplier: 1 - 0.02*float64(shaman.Talents.MentalQuickness),
-	// 	},
-	// 	Cast: core.CastConfig{
-	// 		DefaultCast: core.Cast{
-	// 			GCD: core.GCDDefault,
-	// 		},
-	// 		CD: core.Cooldown{
-	// 			Timer:    shaman.NewTimer(),
-	// 			Duration: core.BloodlustCD,
-	// 		},
-	// 	},
-	// 	ExtraCastCondition: func(sim *core.Simulation, target *core.Unit) bool {
-	// 		// Need to check if any raid member has lust, not just self, because of
-	// 		// major CD ordering issues with the shared bloodlust.
+		ManaCost: core.ManaCostOptions{
+			BaseCost:   0.26,
+			Multiplier: 1 - 0.02*float64(shaman.Talents.MentalQuickness),
+		},
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: core.GCDDefault,
+			},
+			CD: core.Cooldown{
+				Timer:    shaman.NewTimer(),
+				Duration: core.BloodlustCD,
+			},
+		},
+		ExtraCastCondition: func(sim *core.Simulation, target *core.Unit) bool {
+			// Need to check if any raid member has lust, not just self, because of
+			// major CD ordering issues with the shared bloodlust.
 
-	// 		// If all players in all parties already have sated, don't cast.
-	// 		allSated := true
-	// 		for _, party := range shaman.Env.Raid.Parties {
-	// 			for _, partyMember := range party.Players {
-	// 				// If anyone currently has bloodlust, don't cast this.
-	// 				if partyMember.GetCharacter().HasActiveAuraWithTag(core.BloodlustAuraTag) {
-	// 					return false
-	// 				}
-	// 				if !partyMember.GetCharacter().HasActiveAura(core.SatedAuraLabel) {
-	// 					allSated = false
-	// 				}
-	// 			}
-	// 		}
-	// 		return true && !allSated
-	// 	},
+			// If all players in all parties already have sated, don't cast.
+			allSated := true
+			for _, party := range shaman.Env.Raid.Parties {
+				for _, partyMember := range party.Players {
+					// If anyone currently has bloodlust, don't cast this.
+					if partyMember.GetCharacter().HasActiveAuraWithTag(core.BloodlustAuraTag) {
+						return false
+					}
+					if !partyMember.GetCharacter().HasActiveAura(core.SatedAuraLabel) {
+						allSated = false
+					}
+				}
+			}
+			return true && !allSated
+		},
 
-	// 	ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
-	// 		for _, blAura := range blAuras {
-	// 			target := blAura.Unit
-	// 			// Only activate bloodlust on units without bloodlust and without sated.
-	// 			if !target.HasActiveAura(core.SatedAuraLabel) && !target.HasActiveAuraWithTag(core.BloodlustAuraTag) {
-	// 				blAura.Activate(sim)
-	// 			}
+		ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
+			for _, blAura := range blAuras {
+				target := blAura.Unit
+				// Only activate bloodlust on units without bloodlust and without sated.
+				if !target.HasActiveAura(core.SatedAuraLabel) && !target.HasActiveAuraWithTag(core.BloodlustAuraTag) {
+					blAura.Activate(sim)
+				}
 
-	// 		}
-	// 	},
-	// })
+			}
+		},
+	})
 
-	// if !shaman.IsUsingAPL {
-	// 	shaman.AddMajorCooldown(core.MajorCooldown{
-	// 		Spell:    bloodlustSpell,
-	// 		Priority: core.CooldownPriorityBloodlust,
-	// 		Type:     core.CooldownTypeDPS,
-	// 	})
-	// }
+	if !shaman.IsUsingAPL {
+		shaman.AddMajorCooldown(core.MajorCooldown{
+			Spell:    bloodlustSpell,
+			Priority: core.CooldownPriorityBloodlust,
+			Type:     core.CooldownTypeDPS,
+		})
+	} */
 }
--- sim/shaman/feral_spirit.go
+++ sim/shaman/feral_spirit.go
@@ -1,50 +1,49 @@
 package shaman
 
 func (shaman *Shaman) registerFeralSpirit() {
-	return
-	// if !shaman.Talents.FeralSpirit {
-	// 	return
-	// }
+	/* if !shaman.Talents.FeralSpirit {
+		return
+	}
 
-	// spiritWolvesActiveAura := shaman.RegisterAura(core.Aura{
-	// 	Label:    "Feral Spirit",
-	// 	ActionID: core.ActionID{SpellID: 51533},
-	// 	Duration: time.Second * 45,
-	// })
+	spiritWolvesActiveAura := shaman.RegisterAura(core.Aura{
+		Label:    "Feral Spirit",
+		ActionID: core.ActionID{SpellID: 51533},
+		Duration: time.Second * 45,
+	})
 
-	// shaman.FeralSpirit = shaman.RegisterSpell(core.SpellConfig{
-	// 	ActionID: core.ActionID{SpellID: 51533},
+	shaman.FeralSpirit = shaman.RegisterSpell(core.SpellConfig{
+		ActionID: core.ActionID{SpellID: 51533},
 
-	// 	ManaCost: core.ManaCostOptions{
-	// 		BaseCost: 0.12,
-	// 	},
-	// 	Cast: core.CastConfig{
-	// 		DefaultCast: core.Cast{
-	// 			GCD: core.GCDDefault,
-	// 		},
-	// 		IgnoreHaste: true,
-	// 		CD: core.Cooldown{
-	// 			Timer:    shaman.NewTimer(),
-	// 			Duration: time.Minute * 3,
-	// 		},
-	// 	},
+		ManaCost: core.ManaCostOptions{
+			BaseCost: 0.12,
+		},
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				GCD: core.GCDDefault,
+			},
+			IgnoreHaste: true,
+			CD: core.Cooldown{
+				Timer:    shaman.NewTimer(),
+				Duration: time.Minute * 3,
+			},
+		},
 
-	// 	ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
-	// 		shaman.SpiritWolves.EnableWithTimeout(sim)
-	// 		shaman.SpiritWolves.CancelGCDTimer(sim)
+		ApplyEffects: func(sim *core.Simulation, _ *core.Unit, _ *core.Spell) {
+			shaman.SpiritWolves.EnableWithTimeout(sim)
+			shaman.SpiritWolves.CancelGCDTimer(sim)
 
-	// 		// Add a dummy aura to show in metrics
-	// 		spiritWolvesActiveAura.Activate(sim)
+			// Add a dummy aura to show in metrics
+			spiritWolvesActiveAura.Activate(sim)
 
-	// 		// https://github.com/JamminL/wotlk-classic-bugs/issues/280
-	// 		// instant casts (e.g. shocks) usually don't reset a shaman's swing timer
-	// 		shaman.AutoAttacks.StopMeleeUntil(sim, sim.CurrentTime, false)
-	// 	},
-	// })
+			// https://github.com/JamminL/wotlk-classic-bugs/issues/280
+			// instant casts (e.g. shocks) usually don't reset a shaman's swing timer
+			shaman.AutoAttacks.StopMeleeUntil(sim, sim.CurrentTime, false)
+		},
+	})
 
-	// shaman.AddMajorCooldown(core.MajorCooldown{
-	// 	Spell:    shaman.FeralSpirit,
-	// 	Priority: core.CooldownPriorityDrums + 1000, // Always prefer to use wolves before bloodlust/drums so wolves gain haste buff
-	// 	Type:     core.CooldownTypeDPS,
-	// })
+	shaman.AddMajorCooldown(core.MajorCooldown{
+		Spell:    shaman.FeralSpirit,
+		Priority: core.CooldownPriorityDrums + 1000, // Always prefer to use wolves before bloodlust/drums so wolves gain haste buff
+		Type:     core.CooldownTypeDPS,
+	}) */
 }
